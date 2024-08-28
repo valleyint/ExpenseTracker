792b35f97717f81aec15ab15fb8fc71769c14239
TS <tushar.sudeep@gmail.com>
2024-08-28 22:58:51 +0530

    multiple bugs are present

--- Simplified.py
+++ Simplified.py
@@ -17,7 +17,7 @@ class ExpensesTracker():
         self.income_amount = [100.0, 300.0, 500.0]
         self.expense_name = ["food1", "food2", "food3"]
         self.expense_amount = [200.0, 400.0, 600.0]
-        self.button_names = [0, "INCOME", "EXPENSE", "GRAPHS", "QUIT"]
+        self.button_names = [1, "INCOME", "EXPENSE", "GRAPHS", "QUIT"]
         self.button_list = []
         self.income_or_expense = ""
         self.root.title("Expenses Tracker")
@@ -31,6 +31,7 @@ class ExpensesTracker():
         self.label1.pack(pady = 50)
         self.frame1.pack(anchor = "n", fill = "x")
         self.frame2 = ttkb.Frame(self.root)
+        self.entry = ttkb.Entry(self.root)
         self.create_buttons()
         self.frame2.pack(anchor = "n", fill = "x")
 
@@ -43,35 +44,67 @@ class ExpensesTracker():
             self.button_list[i-1].pack(side = "left", padx = 100, pady = 5)    
 
     def button_command(self, seq, num):
-        if seq == "0":
+        if seq == "1":
             if num == 1:
                 self.income_or_expense = "Income"
-                self.button_names = [1, "↩", "ADD INCOME", "REMOVE INCOME", "UPDATE INCOME", "GET INCOME"]
+                self.button_names = [11, "↩", "ADD INCOME", "REMOVE INCOME", "UPDATE INCOME", "GET INCOME"]
                 self.create_buttons()
             elif num == 2:
                 self.income_or_expense = "Expense"
+                self.button_names = [12, "↩", "ADD EXPENSE", "REMOVE EXPENSE", "UPDATE EXPENSE", "GET EXPENSE"]
+                self.create_buttons()
             elif num == 3:
-                self.pie_chart(input("Do you wish to see:\n1. Income chart\n2. Expense chart\n\nChoice: "))
+                self.button_names = [13, "↩", "INCOME CHART", "EXPENSE CHART"]
+                self.create_buttons()
             else:
                 root.destroy()
-        if seq == "1":
+        if seq == "11" or seq == "12":
             if num == 1:
-                self.button_names = [0, "INCOME", "EXPENSE", "GRAPHS", "QUIT"]
+                self.button_names = [1, "INCOME", "EXPENSE", "GRAPHS", "QUIT"]
+                self.label_output.destroy()
+                self.label_entry1.destroy()
                 self.create_buttons()
             elif num == 2:
-                name = input("Enter income name: ")
-                amount = float(input("Enter income amount: "))
-                self.add(name, amount)
+                self.label_entry1 = ttkb.Label(self.root, text = f"Enter {self.income_or_expense} name: ")
+                self.entry1 = ttkb.Entry(self.root)
+                self.label_entry2 = ttkb.Label(self.root, text = f"Enter {self.income_or_expense} amount: ")
+                self.entry2 = ttkb.Entry(self.root)
+                self.button1 = ttkb.Button(self.root, text = "Submit", command = self.submit, bootstyle = ("warning", "outline"))
+                self.label_entry1.pack()
+                self.entry1.pack()
+                self.label_entry2.pack()
+                self.entry2.pack()
+                self.button1.pack()
             elif num == 3:
-                name = input("Enter income name: ")
+                name = input(f"Enter {self.income_or_expense} name: ")
                 self.remove(name)
             elif num == 4:
-                name = input("Enter income name: ")
-                amount = float(input("Enter new income amount: "))
+                name = input(f"Enter {self.income_or_expense} name: ")
+                amount = float(input(f"Enter new {self.income_or_expense} amount: "))
                 self.update(name, amount)
             else:
-                name = input("Enter income name: ")
-                self.show(name)
+                self.label_entry1 = ttkb.Label(self.root, text = f"Enter {self.income_or_expense} name: ")
+                self.entry1 = ttkb.Entry(self.root)
+                self.button1 = ttkb.Button(self.root, text = "Submit", command = self.show)
+                self.label_entry1.pack()
+                self.entry1.pack()
+                self.button1.pack()
+        if seq == "13":
+            if num == 1:
+                self.button_names = [1, "INCOME", "EXPENSE", "GRAPHS", "QUIT"]
+                self.create_buttons()
+            else:
+                self.pie_chart(num)
+
+    def submit(self):
+        name = str(self.entry1.get())
+        amount = float(self.entry2.get())
+        self.entry1.destroy()
+        self.entry2.destroy()
+        self.label_entry1.configrure(text = "Successfully added!")
+        self.label_entry2.destroy()
+        self.add(name, amount)
+        self.button1.destroy()
 
     def add(self, name, amount):
         if self.income_or_expense == "Income":
@@ -116,40 +149,30 @@ class ExpensesTracker():
                 print("Expense not found.")
         print(self.income_or_expense, "has been updated successfully.")
 
-    def show(self, name):
-        if name in self.income_name or name in self.expense_name or name == "everything":
+    def show(self):
+        self.name = self.entry1.get()
+        self.label_entry1.destroy()
+        self.entry1.destroy()
+        self.button1.destroy()
+        if self.name in self.income_name or self.name in self.expense_name:
             if self.income_or_expense == "Income":
-                if name == "everything":
-                    print("-------------------------------------")
-                    for i in range(len(self.income_name)):
-                        print(self.income_name[i], ":", self.income_amount[i])
-                    print("-------------------------------------")
-
-                else:
-                    print(name, ":", self.income_amount[self.income_name.index(name)])
+                self.show_output = self.name + " : " + str(self.income_amount[self.income_name.index(self.name)])
             else:
-                if name == "everything":
-                    print("-------------------------------------")
-                    for i in range(len(self.expense_name)):
-                        print(self.expense_name[i], ":", self.expense_amount[i])
-                    print("-------------------------------------")
-
-                else:
-                    print(name, ":", self.expense_amount[self.expense_name.index(name)])
+                self.show_output = self.name + " : " + str(self.expense_amount[self.expense_name.index(self.name)])
         else:
-            print(self.income_or_expense, "not found.")
+            self.show_output = self.income_or_expense + " not found."
+        self.label_output = ttkb.Label(self.root, text = self.show_output)
+        self.label_output.pack()
 
     def pie_chart(self, type):
             self.canvas = ttkb.Canvas()
             plot.style.use("dark_background")
-            if type == "1":
+            if type == 2:
                 plot.pie(self.income_amount, labels = self.income_name, wedgeprops=dict(width=0.5))
                 plot.title("INCOME GRAPH")
-            elif type == "2":
+            if type == 3:
                 plot.pie(self.expense_amount, labels = self.expense_name, wedgeprops=dict(width=0.5))
                 plot.title("EXPENSE GRAPH")
-            else:
-                print("Error")
             plot.show()
             self.canvas.pack()
 
