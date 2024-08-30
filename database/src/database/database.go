7fe062deb24d6c77e979785a4889f85077baf5e0
darshan <darshan@melkote.com>
2024-08-30 18:53:39 +0530

    dded db with gorm

--- database/src/database/database.go
+++ database/src/database/database.go
@@ -1,55 +1,61 @@
 package database
 
 import (
-	"context"
-	"database/sql"
-	"log"
+	"time"
 
-	_ "modernc.org/sqlite"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-const (
-	dbpath = ""
-)
-
-type db struct {
-	db *sql.DB
+type Db struct {
+	db *gorm.DB
 }
 
+type Income struct {
+	gorm.Model
+	Tm    time.Time `gorm:"primaryKey"`
+	Name  string    `gorm:"primaryKey"`
+	Value float64
+	// expense []*income `gorm:"many2many:expense_incomes;foreignKey"`
+}
 
-func OpenDB () *db {
-	d := db{}
+type Expense struct {
+	gorm.Model
+	Tm    time.Time `gorm:"primaryKey"`
+	Name  string    `gorm:"primaryKey"`
+	Value float64
+	// incomes []*income `gorm:"many2many:income_expenses;"`
+}
+
+func CreateFianances() (*Db, error) {
+	d := Db{}
 	var err error
-	d.db ,err = sql.Open("sqlite",dbpath)
+
+	d.db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
-		log.Println(err)
-		return nil
+		return nil, err
 	}
 
-	_, err = d.db.ExecContext(context.Background(), `
-		CREATE TABLE IF NOT EXISTS finance {
-			date DATE NOT NULL,
-			time TIME NOT NULL
-			PRIMARY KEY (date,time)
-		}
-	
-		CREATE TABLE IF NOT EXISTS income {
-			date  DATE			NOT NULL,
-			time  TIME 			NOT NULL,
-			name  VARCHAR(100)	NOT NULL,
-			value INT			NOT NULL
-			PRIMARY KEY (date,time,name)
-			FOREIGN KEY (date,time) REFERENCES fiance (date,time)
-		} 
-		
-		CREATE TABLE IF NOT EXISTS expense {
-			date  DATE			NOT NULL,
-			time  TIME 			NOT NULL,
-			name  VARCHAR(100)	NOT NULL,
-			value INT			NOT NULL
-			PRIMARY KEY (date,time,name)
-			FOREIGN KEY (date,time) REFERENCES fiance (date,time)
-		} 
-	`)
-	return &db{}
+	d.db.AutoMigrate(&Income{}, &Expense{})
+
+	return &d, nil
 }
+
+func (d *Db) AddIncome(i Income) {
+	d.db.Create(&i)
+}
+
+func (d *Db) AddExpense(e Expense) {
+	d.db.Create(&e)
+}
+
+func (d *Db) GetAtTime(tm time.Time) ([]Income, []Expense) {
+	var incomes []Income
+	d.db.Raw("SELECT tm, name, value FROM incomes WHERE tm = ?", tm).Scan(&Income{})
+
+	var expenses []Expense
+	d.db.Raw("SELECT tm, name, value FROM expenses WHERE tm = ?", tm).Scan(&Expense{})
+
+	return incomes, expenses
+}
+
---
+++ database/src/database/database_test.go
@@ -0,0 +1,25 @@
+package database_test
+
+import (
+	"expensetracker/database"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestDatabase (t *testing.T) {
+	d, err := database.CreateFianances()
+	if err != nil {
+		panic(err)
+	}
+
+	now := time.Now()
+	for loop := 0; loop < 5; loop++ {
+		d.AddExpense(database.Expense{Tm: now.Add(time.Duration(int64(time.Second) * int64(loop))), Name: "test", Value: float64(loop)})
+		d.AddIncome(database.Income{Tm: now.Add(time.Duration(int64(time.Second) * int64(loop))), Name: "test", Value: float64((loop)) * 2})
+	}
+
+	for loop := 0; loop < 10; loop++ {
+		fmt.Println(d.GetAtTime(now.Add(time.Duration(int64(time.Second) * int64(loop)))))
+	}
+}
