4093f4bdae0ec8a21d5f17d08ae0412bcc77d070
TS <tushar.sudeep@gmail.com>
2025-01-14 15:06:41 +0530

    Clean up

--- testDbConnector.py
+++
@@ -1,9 +0,0 @@
-import db_alchemy as db
-import time
-
-d = db.Db()
-d.create_tables()
-d.add_expense("aex",100,time.time())
-d.add_income("ain",100,time.time())
-
-print(d.get_income('ain'),d.get_expense('aex'),sep='\n')
